d9a7ea6670efd4ec8c61c961ff3629a0c8b727bd
Niv Sardi <xaiki@evilgiggle.com>
2019-06-12 23:12:10 -0300

    update downloadAndInsertOpenCV.sh for OCV4.0
    we make it a tad easier to update too.

--- downloadAndInsertOpenCV.sh
+++ downloadAndInsertOpenCV.sh
@@ -1,21 +1,25 @@
-# ios
+# taken from https://github.com/brainhubeu/react-native-opencv-tutorial/blob/master/downloadAndInsertOpenCV.sh
 
-wget https://sourceforge.net/projects/opencvlibrary/files/opencv-ios/3.4.1/opencv-3.4.1-ios-framework.zip
-unzip -a opencv-3.4.1-ios-framework.zip
+# ios
+version=4.1.0
+base_url=https://razaoinfo.dl.sourceforge.net/project/opencvlibrary/${version}/
+
+wget ${base_url}/opencv-${version}-ios-framework.zip
+unzip -a opencv-${version}-ios-framework.zip
 cd ios
 cp -r ./../opencv2.framework ./
 cd ..
-rm -rf opencv-3.4.1-ios-framework.zip
+rm -rf opencv-${version}-ios-framework.zip
 rm -rf opencv2.framework/
 
 # android
 
-wget https://sourceforge.net/projects/opencvlibrary/files/opencv-android/3.4.1/opencv-3.4.1-android-sdk.zip
-unzip opencv-3.4.1-android-sdk.zip
+wget ${base_url}/opencv-${version}-android-sdk.zip
+unzip opencv-${version}-android-sdk.zip
 cd android/app/src/main
 mkdir jniLibs
 cp -r ./../../../../OpenCV-android-sdk/sdk/native/libs/ ./jniLibs
 cd ../../../../
-rm -rf opencv-3.4.1-android-sdk.zip
+rm -rf opencv-${version}-android-sdk.zip
 rm -rf OpenCV-android-sdk/
 
