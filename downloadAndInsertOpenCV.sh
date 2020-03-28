1faf5a1b76672f9d9ba1c738df06e0f2bd40f858
Rohan4045 <52060192+Rohan4045@users.noreply.github.com>
2020-03-28 17:28:19 +0530

    Update downloadAndInsertOpenCV.sh

--- downloadAndInsertOpenCV.sh
+++ downloadAndInsertOpenCV.sh
@@ -2,24 +2,24 @@
 
 # ios
 version=4.1.0
-base_url=https://razaoinfo.dl.sourceforge.net/project/opencvlibrary/${version}/
+base_url=https://razaoinfo.dl.sourceforge.net/project/opencvlibrary/${3.4.9}/
 
-wget ${base_url}/opencv-${version}-ios-framework.zip
-unzip -a opencv-${version}-ios-framework.zip
-cd ios
-cp -r ./../opencv2.framework ./
-cd ..
-rm -rf opencv-${version}-ios-framework.zip
-rm -rf opencv2.framework/
+# wget ${base_url}/opencv-${version}-ios-framework.zip
+# unzip -a opencv-${version}-ios-framework.zip
+# cd ios
+# cp -r ./../opencv2.framework ./
+# cd ..
+# rm -rf opencv-${version}-ios-framework.zip
+# rm -rf opencv2.framework/
 
-# android
+# # android
 
-wget ${base_url}/opencv-${version}-android-sdk.zip
-unzip opencv-${version}-android-sdk.zip
+wget ${base_url}/opencv-${3.4.9}-android-sdk.zip
+unzip opencv-${3.4.9}-android-sdk.zip
 cd android/app/src/main
 mkdir jniLibs
 cp -r ./../../../../OpenCV-android-sdk/sdk/native/libs/ ./jniLibs
 cd ../../../../
-rm -rf opencv-${version}-android-sdk.zip
+rm -rf opencv-${3.4.9}-android-sdk.zip
 rm -rf OpenCV-android-sdk/
 
